dfa57023cfe28f9af2bfa0781c2e0ca30f9d2fe7
Carlos Felipe <rezendegood120@gmail.com>
2025-12-22 12:02:01 -0300

    Delete src/middlewares/passportAuth.ts

--- src/middlewares/passportAuth.ts
+++
@@ -1,4 +0,0 @@
-import passport from './passportConfig.js'; // Importa aquele config que criamos antes
-
-// Exporta apenas o middleware do passport
-export const requireAuth = passport.authenticate('jwt', { session: false });
